d84bbc99f0a5e6a214ee174a96d9cfc86f4d0ec1
Mimezzz <78015770+Mimezzz@users.noreply.github.com>
2021-08-21 18:12:52 +1000

    final edition

--- pyPoll/main.py
+++ pyPoll/main.py
@@ -1,8 +1,7 @@
 import csv
 
 total_votes=0
-candidate_list=[]
-candidate_vote=[]
+candidate_dic={}
 vote_percentage=[]
 max_vote=0
 with open('resources/election_data.csv','r') as csvfile:
@@ -10,30 +9,27 @@ with open('resources/election_data.csv','r') as csvfile:
     header=next(csvfile)
     for row in csvread:
         total_votes+=1
-        if row[2] not in candidate_list:
-            candidate_list.append(row[2])
-            candidate_vote.append(0)
-        for i in range(len(candidate_list)):
-            if row[2]==candidate_list[i]:
-                candidate_vote[i]+=1
-    for i in range(len(candidate_list)):
-        vote_percentage.append(round((candidate_vote[i]/total_votes*100),2))
-        if candidate_vote[i]>max_vote:
-            max_vote=candidate_vote[i]
-            winner=candidate_list[i]
+        if row[2] not in candidate_dic.keys():
+            candidate_dic[row[2]]=0
+        candidate_dic[row[2]]+=1
 
-print('Election Results \n-------------------------')
-print(f'Total Votes: {total_votes}\n-------------------------')
-for i in range(len(candidate_list)):
-    print(f'{candidate_list[i]}:{vote_percentage[i]}% ({candidate_vote[i]})')
-print(f'-------------------------\nWinner: {winner}\n-------------------------')
+lines=[
+        'Election Results \n-------------------------\n',
+        f'Total Votes: {total_votes}\n-------------------------\n'
+    ]
 
+for keys,values in candidate_dic.items():
+    line= (f'{keys}: {round((values/total_votes*100),2)} % ({values})\n')
+    lines.append(line)
+    if values>max_vote:
+        max_vote=values
+        winner=keys
 
-print('Election Results \n-------------------------',file=open("analysis/analysis.txt", "a"))
-print(f'Total Votes: {total_votes}\n-------------------------',file=open("analysis/analysis.txt", "a"))
-for i in range(len(candidate_list)):
-    print(f'{candidate_list[i]}:{vote_percentage[i]}% ({candidate_vote[i]})',file=open("analysis/analysis.txt", "a"))
-print(f'-------------------------\nWinner: {winner}\n-------------------------',file=open("analysis/analysis.txt", "a"))
+lines.append(f'-------------------------\nWinner: {winner}\n-------------------------')
 
+with open('analysis/analysis.txt', 'w', newline='') as t:
+    t.writelines(lines)
 
-
+with open('analysis/analysis.txt','r') as f:
+    for x in f:
+        print(x)
