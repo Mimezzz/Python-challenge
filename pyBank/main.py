668e5dcb094d00c34e098bb986bdf442ed064297
Mimezzz <78015770+Mimezzz@users.noreply.github.com>
2021-08-26 22:11:21 +1000

    Update main.py

--- pyBank/main.py
+++ pyBank/main.py
@@ -6,16 +6,23 @@ total_change=0
 max_increase=0
 max_decrease=0
 
+# read file
 with open('resources/budget_data.csv', 'r') as csvfile:
     csvread=csv.reader(csvfile,delimiter=',')
-
+# The total number of months included in the dataset
     csvheader=next(csvread)
     for row in csvread:
         total_month+=1
+# The net total amount of "Profit/Losses" over the entire period
         total=total+float(row[1])
+# The average of the changes in "Profit/Losses" over the entire period     
         new_change=float(row[1])-last_figure
         last_figure = float(row[1])
         total_change+=new_change
+# The greatest increase in profits (date and amount) over the entire period
+
+# The greatest decrease in losses (date and amount) over the entire period
+
         if new_change>max_increase:
             max_increase=new_change
             max_increase_time=row[0]
@@ -23,7 +30,7 @@ with open('resources/budget_data.csv', 'r') as csvfile:
             max_decrease=new_change
             max_decrease_time=row[0]
 
-
+# print results
 
     avg_change=round(total_change/(total_month-1),2)
     print('Financial Analysis\n---------------')
@@ -33,6 +40,7 @@ with open('resources/budget_data.csv', 'r') as csvfile:
     print(f'Greatest Increase in Profits: {max_increase_time} {max_increase}')
     print(f'Greatest Decrease in Profits: {max_decrease_time} {max_decrease}')
 
+#     write new file
 with open('analysis/analysis.txt','w',newline='') as newtxt:
     lines=[
         'Financial Analysis\n---------------',
