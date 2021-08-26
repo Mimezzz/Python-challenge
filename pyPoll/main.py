d53e5d8be0e2b7750f4ad658310341d3a0f0773b
Mimezzz <78015770+Mimezzz@users.noreply.github.com>
2021-08-26 22:13:44 +1000

    with comments

--- pyPoll/main.py
+++ pyPoll/main.py
@@ -4,19 +4,27 @@ total_votes=0
 candidate_dic={}
 vote_percentage=[]
 max_vote=0
+
+# read file
 with open('resources/election_data.csv','r') as csvfile:
     csvread=csv.reader(csvfile,delimiter=',')
     header=next(csvfile)
     for row in csvread:
         total_votes+=1
+# list of candidates who received votes
         if row[2] not in candidate_dic.keys():
             candidate_dic[row[2]]=0
         candidate_dic[row[2]]+=1
 
+#  The total number of votes each candidate won
+        
 lines=[
         'Election Results \n-------------------------\n',
         f'Total Votes: {total_votes}\n-------------------------\n'
     ]
+
+# The percentage of votes each candidate won
+# The winner of the election based on popular vote.
 
 for keys,values in candidate_dic.items():
     line= (f'{keys}: {round((values/total_votes*100),2)} % ({values})\n')
@@ -27,9 +35,11 @@ for keys,values in candidate_dic.items():
 
 lines.append(f'-------------------------\nWinner: {winner}\n-------------------------')
 
+# write file
 with open('analysis/analysis.txt', 'w', newline='') as t:
     t.writelines(lines)
-
+    
+# read/print results
 with open('analysis/analysis.txt','r') as f:
     for x in f:
         print(x)
